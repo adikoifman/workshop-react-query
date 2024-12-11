05d86c8fb2526c5c781f12439590be2b70f686a7
adi-koifman <adikoifman@hilma.tech>
2024-12-11 13:06:14 +0200

    hay

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Todos } from "./components/Todos";
 
+const queryClient = new QueryClient();
+
+//all of this bc need to use query hooks ... wtw
 function App() {
   return (
     <>
-      <h1>TODOs</h1>
-      <Todos />
+      <QueryClientProvider client={queryClient}>
+        <h1>TODOs</h1>
+        <Todos />
+      </QueryClientProvider>
     </>
   );
 }
--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -3,23 +3,29 @@ import { TodoCard } from "./TodoCard";
 import { Todo } from "../common/types/todo.interface";
 import { useEffect, useState } from "react";
 import { AddTodo } from "./AddTodo";
+import { useQuery } from "@tanstack/react-query";
 
 export function Todos() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const { data: todos , error } = useQuery({
+    queryKey: ["todos"], 
+    queryFn: fetchTodo,
+  });
+  const [_todos, setTodos] = useState<Todo[]>([]);
 
-  useEffect(() => {
-    fetchTodo();
-  }, []);
+  // useEffect(() => {
+  //   fetchTodo();
+  // }, []);
 
   async function fetchTodo() {
-    const { data } = await axios.get<Todo[]>("/api/todos");
-    setTodos(data);
+    const { data: fetchedData } = await axios.get<Todo[]>("/api/todos");
+    // setTodos(data);
+    return fetchedData;
   }
 
   return (
     <>
     <div className="todo-container">
-      {todos.map((todo, index) => (
+      {todos?.map((todo, index) => (
         <TodoCard todo={todo} setTodos={setTodos} index={index} key={todo.id} />
       ))}
     </div>
